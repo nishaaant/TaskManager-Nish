01022a5b1c2f4bbcb71dcce4ed81c7a1ea28f3fb
nishaaant <nishaaant1608@gmail.com>
2025-01-25 17:10:51 +0530

    Task Model

---
+++ server/src/models/Task.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    startTime: { type: Date, required: true },
+    endTime: { type: Date, required: true },
+    priority: { type: Number, required: true, min: 1, max: 5 },
+    status: { type: String, required: true, enum: ["pending", "finished"] },
+  },
+  { timestamps: true }
+);
+
+const TaskModel = mongoose.model("Task", taskSchema);
+export default TaskModel;
